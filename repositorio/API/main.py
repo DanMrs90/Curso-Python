0bdc51df1c9982c269650817f4aa58f3616ca548
DanMrs90 <fdqm_daniel@hotmail.com>
2023-08-03 21:08:25 -0600

    APIS Y BASE DE DATOS

--- repositorio/API/main.py
+++ repositorio/API/main.py
@@ -1,11 +1,29 @@
 from  fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
 
 app=FastAPI()
+
+
+class Libro(BaseModel):
+    titulo:str
+    autor:str
+    paginas:int
+    editorial:Optional[str]
+
 @app.get("/")
-def raiz():
+async def raiz():
     return {"Hola":"Mundo"}
 
-@app.get("/items/{item_id}/{m}")
+@app.get("/libros/{id}")
+async def mostrar_libros(id:int):
+    return{"data":id}
 
+@app.post("/libros/")
+async def insertar_libro(libro:Libro):
+    return{f"El libro {libro.titulo} se ha insertado correctamente"}
+
+
+@app.get("/items/{item_id}/{m}")
 def read_item(item_id,m:str=None):
     return {"Item id":item_id,"m":m}
